a68e67c5b9b65ed5a5d32d8b0977709c2c28065d
Stanislav Vin <vinninjaq@gmail.com>
2025-08-25 00:04:00 +0200

    feat/contract: commit/reveal test

--- test/Vault.test.ts
+++ test/Vault.test.ts
@@ -10,6 +10,7 @@ describe('Vault', function () {
   let someAddr: Signer
   let ct: Vault
   const ETH_AMOUNT = ethers.parseEther('100')
+  const SECRET_REVEAL_PHRASE = 'secret'
 
   beforeEach(async function () {
     ;[owner, addr1, someAddr] = await ethers.getSigners()
@@ -198,6 +199,213 @@ describe('Vault', function () {
       const addr1Deposit = await ct.connect(addr1).getUserDeposit()
 
       expect(addr1Deposit).to.be.eq(ETH_AMOUNT)
+    })
+  })
+
+  describe('Commit/reveal user withdraw', function () {
+    describe('commit', function () {
+      it('Should be able to commit', async function () {
+        const addr1Address = await addr1.getAddress()
+
+        await addr1.sendTransaction({
+          to: ct.getAddress(),
+          value: ETH_AMOUNT,
+        })
+
+        const hashedSecretPhrase =
+          ethers.encodeBytes32String(SECRET_REVEAL_PHRASE)
+        const hashedWithdrawCommit = ethers.solidityPackedKeccak256(
+          ['address', 'bytes32', 'uint256'],
+          [addr1Address, hashedSecretPhrase, ETH_AMOUNT],
+        )
+
+        await ct.connect(addr1).commitUserWithdraw(hashedWithdrawCommit)
+
+        const userCommit = await ct.userWithdrawCommits(addr1Address)
+        expect(userCommit).to.eq(hashedWithdrawCommit)
+      })
+
+      it('Revert custom error if commit already exist', async () => {
+        const addr1Address = await addr1.getAddress()
+
+        await addr1.sendTransaction({
+          to: ct.getAddress(),
+          value: ETH_AMOUNT,
+        })
+
+        const hashedSecretPhrase =
+          ethers.encodeBytes32String(SECRET_REVEAL_PHRASE)
+        const hashedWithdrawCommit = ethers.solidityPackedKeccak256(
+          ['address', 'bytes32', 'uint256'],
+          [addr1Address, hashedSecretPhrase, ETH_AMOUNT],
+        )
+
+        await ct.connect(addr1).commitUserWithdraw(hashedWithdrawCommit)
+
+        expect(ct.connect(addr1).commitUserWithdraw(hashedWithdrawCommit))
+          .to.be.revertedWithCustomError(ct, 'ExistingUserWithdrawCommit')
+          .withArgs('Commit already exist')
+      })
+
+      it('Should emit event CreatedUserWithdrawEvent', async () => {
+        const addr1Address = await addr1.getAddress()
+
+        await addr1.sendTransaction({
+          to: ct.getAddress(),
+          value: ETH_AMOUNT,
+        })
+
+        const hashedSecretPhrase =
+          ethers.encodeBytes32String(SECRET_REVEAL_PHRASE)
+        const hashedWithdrawCommit = ethers.solidityPackedKeccak256(
+          ['address', 'bytes32', 'uint256'],
+          [addr1Address, hashedSecretPhrase, ETH_AMOUNT],
+        )
+
+        const tx = await ct
+          .connect(addr1)
+          .commitUserWithdraw(hashedWithdrawCommit)
+        await tx.wait()
+
+        expect(tx).to.emit(ct, 'CreatedUserWithdrawEvent')
+      })
+    })
+
+    describe('delete  commit', function () {
+      it('Should delete existing commit', async () => {
+        const addr1Address = await addr1.getAddress()
+
+        await addr1.sendTransaction({
+          to: ct.getAddress(),
+          value: ETH_AMOUNT,
+        })
+
+        const hashedSecretPhrase =
+          ethers.encodeBytes32String(SECRET_REVEAL_PHRASE)
+        const hashedWithdrawCommit = ethers.solidityPackedKeccak256(
+          ['address', 'bytes32', 'uint256'],
+          [addr1Address, hashedSecretPhrase, ETH_AMOUNT],
+        )
+
+        await ct.connect(addr1).commitUserWithdraw(hashedWithdrawCommit)
+
+        await ct.connect(addr1).deleteUserWithdrawCommit()
+        const userCommitEmpty = await ct.userWithdrawCommits(addr1Address)
+
+        expect(userCommitEmpty).to.eq(ethers.ZeroHash)
+      })
+
+      it('Revert with custom error if userWithdrawCommits[msg.sender] empty', async () => {
+        expect(async () => {
+          await ct.connect(addr1).deleteUserWithdrawCommit()
+        })
+          .to.be.revertedWithCustomError(ct, 'NonExistingUserWithdrawCommit')
+          .withArgs('Commit not exist')
+      })
+
+      it('Should emit event DeletedUserWithdrawEvent', async () => {
+        const addr1Address = await addr1.getAddress()
+
+        await addr1.sendTransaction({
+          to: ct.getAddress(),
+          value: ETH_AMOUNT,
+        })
+
+        const hashedSecretPhrase =
+          ethers.encodeBytes32String(SECRET_REVEAL_PHRASE)
+        const hashedWithdrawCommit = ethers.solidityPackedKeccak256(
+          ['address', 'bytes32', 'uint256'],
+          [addr1Address, hashedSecretPhrase, ETH_AMOUNT],
+        )
+
+        await ct.connect(addr1).commitUserWithdraw(hashedWithdrawCommit)
+
+        const tx = await ct.connect(addr1).deleteUserWithdrawCommit()
+
+        expect(tx).to.emit(ct, 'DeletedUserWithdrawEvent')
+      })
+    })
+
+    describe('reveal', function () {
+      it('Should decreese ct balance after reveal', async () => {
+        const addr1Address = await addr1.getAddress()
+        const ctBalanceBeforeTx = await ct.getBalance()
+
+        await addr1.sendTransaction({
+          to: ct.getAddress(),
+          value: ETH_AMOUNT,
+        })
+
+        const hashedSecretPhrase =
+          ethers.encodeBytes32String(SECRET_REVEAL_PHRASE)
+        const hashedWithdrawCommit = ethers.solidityPackedKeccak256(
+          ['address', 'bytes32', 'uint256'],
+          [addr1Address, hashedSecretPhrase, ETH_AMOUNT],
+        )
+
+        await ct.connect(addr1).commitUserWithdraw(hashedWithdrawCommit)
+
+        await ct
+          .connect(addr1)
+          .revealUserWithdraw(ETH_AMOUNT, hashedSecretPhrase)
+
+        const ctBalanceAfterTx = await ct.getBalance()
+
+        expect(ctBalanceBeforeTx).to.be.eq(ctBalanceAfterTx)
+      })
+
+      it('Revert incorrect withdraw call', async () => {
+        const incorrectetherAmount = ethers.parseEther('1000')
+        const addr1Address = await addr1.getAddress()
+
+        await addr1.sendTransaction({
+          to: ct.getAddress(),
+          value: ETH_AMOUNT,
+        })
+
+        const hashedSecretPhrase =
+          ethers.encodeBytes32String(SECRET_REVEAL_PHRASE)
+        const hashedWithdrawCommit = ethers.solidityPackedKeccak256(
+          ['address', 'bytes32', 'uint256'],
+          [addr1Address, hashedSecretPhrase, incorrectetherAmount],
+        )
+
+        await ct.connect(addr1).commitUserWithdraw(hashedWithdrawCommit)
+
+        expect(async () => {
+          await ct
+            .connect(addr1)
+            .revealUserWithdraw(incorrectetherAmount, hashedSecretPhrase)
+        })
+          .to.be.revertedWithCustomError(ct, 'InsufficientBalance')
+          .withArgs(ETH_AMOUNT, incorrectetherAmount)
+      })
+
+      it('Should emit event WithdrawEvent', async () => {
+        const addr1Address = await addr1.getAddress()
+
+        await addr1.sendTransaction({
+          to: ct.getAddress(),
+          value: ETH_AMOUNT,
+        })
+
+        const hashedSecretPhrase =
+          ethers.encodeBytes32String(SECRET_REVEAL_PHRASE)
+        const hashedWithdrawCommit = ethers.solidityPackedKeccak256(
+          ['address', 'bytes32', 'uint256'],
+          [addr1Address, hashedSecretPhrase, ETH_AMOUNT],
+        )
+
+        await ct.connect(addr1).commitUserWithdraw(hashedWithdrawCommit)
+
+        const tx = await ct
+          .connect(addr1)
+          .revealUserWithdraw(ETH_AMOUNT, hashedSecretPhrase)
+
+        expect(tx)
+          .to.emit(ct, 'WithdrawEvent')
+          .withArgs(addr1Address, ETH_AMOUNT)
+      })
     })
   })
 })
